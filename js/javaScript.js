b8d4e960b676097302517c5665b12001501a8fe7
Fernando Rosa <stevensfrr@gmail.com>
2017-11-22 01:36:07 +0000

    modify index and add scripts.js Add the twitter button

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,16 +1,21 @@
-
-var background = ["#FFD180", "#FFAB40", "#FF9100", "#FF6D00", "#616161", "#424242", "#212121", "#B9F6CA", "#69F0AE", "#00E676", "#00C853", "#CCFF90", "#B2FF59", "#76FF03", "#64DD17", "#01579B", "#80D8FF", "#40C4FF", "#00B0FF", "#0091EA"]
+var background = ["#FFD180", "#ff4040", "#ceff00", "#FF6D00", "#616161", "#424242", "#212121", "#B9F6CA", "#4d8e6e", "#00E676", "#02382d", "#CCFF90", "#adbe99", "#76FF03", "#d4d6d3", "#01579B", "#ce00ff", "#40C4FF", "#004eff", "#e100ea"]
 var quotes = [];
+var randonNum;
+
 function generator() {
 	var i = Math.floor((Math.random() * 4999) + 1)
-    var c = Math.floor((Math.random() * 19) + 1)
-	document.getElementById("quote").innerHTML = quotes[i].quoteText;
+	var c = Math.floor((Math.random() * 19) + 1)
+	randonNum = i;
+	document.getElementById("quote").innerHTML = "\"" + quotes[i].quoteText + "\"";
 	document.getElementById("author").innerHTML = "Author: " + quotes[i].quoteAuthor;
 	document.body.style.backgroundColor = background[c];
 	document.body.style.transition = "all 0.7s"
-    
 }
 
-$.getJSON("js/quotes.json", function(data){
-    quotes = data;
-}); 
+function twitter() {
+	var text = "\"" + quotes[randonNum].quoteText + "\"" + "\n" + "Author: " + quotes[randonNum].quoteAuthor;
+	window.open('http://twitter.com/share?text=' + encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+}
+$.getJSON("js/quotes.json", function(data) {
+	quotes = data;
+});
